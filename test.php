ba38dd3dfd0f9712bc12e1cb6314c1493aa9adbd
Grace <jiagege@lingjing.com>
2017-09-13 14:51:54 +0800

    修改，提交1次

--- test.php
+++ test.php
@@ -21,34 +21,35 @@
 //    shuffle($majiang);
 //    return $majiang;
 //}
-function faMjTest($room,$game){
-    $players = $room->players;
-    $hu = false;
-    $majiangList = array();
-
-    for($i = 1; $i <= 4 ; $i++) {
-        $uid = $players[$i];
-        $playerMaJiang = array();
-        if($game->zhuang == $uid){
-            $playerMaJiang = [1,2,3,4,5,6,7,8];
-            $game->pengList[$uid] = [8];
-        }else{
-            if(!$hu){
-                $playerMaJiang = [1,2,3,4,5,6,8,9,9,9];
-                $hu = true;
-            }else{
-                $playerMaJiang = [1,2,3,4,5,6,8,9,9,9];
-            }
-        }
-        sort($playerMaJiang);
-        $majiangList[$uid] = $playerMaJiang;
-    }
-
-    $game->currentMj2 = 0;
-    $game->currentMj = 100;
-    return array($game,$majiangList);
-}
-$arr = faMjTest($room,$game);
-echo "<pre>";
-print_r($arr);
-echo "</pre>";
+//function faMjTest($room,$game){
+//    $players = $room->players;
+//    $hu = false;
+//    $majiangList = array();
+//
+//    for($i = 1; $i <= 4 ; $i++) {
+//        $uid = $players[$i];
+//        $playerMaJiang = array();
+//        if($game->zhuang == $uid){
+//            $playerMaJiang = [1,2,3,4,5,6,7,8];
+//            $game->pengList[$uid] = [8];
+//        }else{
+//            if(!$hu){
+//                $playerMaJiang = [1,2,3,4,5,6,8,9,9,9];
+//                $hu = true;
+//            }else{
+//                $playerMaJiang = [1,2,3,4,5,6,8,9,9,9];
+//            }
+//        }
+//        sort($playerMaJiang);
+//        $majiangList[$uid] = $playerMaJiang;
+//    }
+//
+//    $game->currentMj2 = 0;
+//    $game->currentMj = 100;
+//    return array($game,$majiangList);
+//}
+//$arr = faMjTest($room,$game);
+//echo "<pre>";
+//print_r($arr);
+//echo "</pre>";
+echo '修改，提交1次';
